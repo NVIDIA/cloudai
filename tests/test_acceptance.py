b35f84a8ccb02388f8413a3940b9afe6efb84a7f
Andrey Maslennikov <andreyma@nvidia.com>
2024-05-16 12:09:30 +0200

    Remove test that hangs in GHA

--- tests/test_acceptance.py
+++ tests/test_acceptance.py
@@ -9,10 +9,6 @@ SLURM_TEST_SCENARIOS = [
     Path("conf/v0.6/general/test_scenario/ucc_test/test_scenario.toml"),
 ]
 
-STANDALONE_TEST_SCENARIOS = [
-    Path("conf/v0.6/general/test_scenario/sleep/test_scenario.toml"),
-]
-
 
 @pytest.mark.parametrize("test_scenario_path", SLURM_TEST_SCENARIOS, ids=lambda x: str(x))
 def test_slurm(tmp_path: Path, test_scenario_path: Path):
@@ -22,26 +18,6 @@ def test_slurm(tmp_path: Path, test_scenario_path: Path):
         mode="dry-run",
         output_path=str(tmp_path),
         system_config_path="conf/v0.6/general/system/example_slurm_cluster.toml",
-        test_scenario_path=str(test_scenario_path),
-        test_path="conf/v0.6/general/test",
-        test_template_path="conf/v0.6/general/test_template",
-    )
-    handle_dry_run_and_run(args)
-
-    test_dir = list(tmp_path.glob("*"))[0]
-    for td in test_dir.iterdir():
-        assert td.is_dir(), "Invalid test directory"
-        assert "Tests." in td.name, "Invalid test directory name"
-
-
-@pytest.mark.parametrize("test_scenario_path", STANDALONE_TEST_SCENARIOS, ids=lambda x: str(x))
-def test_standalone(tmp_path: Path, test_scenario_path: Path):
-    args = argparse.Namespace(
-        log_file=None,
-        log_level=None,
-        mode="dry-run",
-        output_path=str(tmp_path),
-        system_config_path="conf/v0.6/general/system/standalone_system.toml",
         test_scenario_path=str(test_scenario_path),
         test_path="conf/v0.6/general/test",
         test_template_path="conf/v0.6/general/test_template",
