8ebbd1b46073f081a7bf11d83a275a48528d947b
Andrey Maslennikov <andreyma@nvidia.com>
2024-05-21 16:27:15 +0200

    Register strategies via Registry

--- src/cloudai/_core/registry.py
+++ src/cloudai/_core/registry.py
@@ -1,7 +1,12 @@
-from typing import Dict, Type
+from typing import Dict, List, Tuple, Type, Union
 
 from cloudai.parser.core.base_system_parser import BaseSystemParser
 from cloudai.runner.core.base_runner import BaseRunner
+from cloudai.schema.core.strategy.job_id_retrieval_strategy import JobIdRetrievalStrategy
+from cloudai.schema.core.strategy.report_generation_strategy import ReportGenerationStrategy
+from cloudai.schema.core.strategy.test_template_strategy import TestTemplateStrategy
+from cloudai.schema.core.system import System
+from cloudai.schema.core.test_template import TestTemplate
 
 
 class Singleton(type):
@@ -20,6 +25,14 @@ class Registry(metaclass=Singleton):
 
     system_parsers_map: Dict[str, Type[BaseSystemParser]] = {}
     runners_map: Dict[str, Type[BaseRunner]] = {}
+    strategies_map: Dict[
+        Tuple[
+            Type[Union[TestTemplateStrategy, ReportGenerationStrategy, JobIdRetrievalStrategy]],
+            Type[System],
+            Type[TestTemplate],
+        ],
+        Type[Union[TestTemplateStrategy, ReportGenerationStrategy, JobIdRetrievalStrategy]],
+    ] = {}
 
     def add_system_parser(self, name: str, value: Type[BaseSystemParser]) -> None:
         """
@@ -45,7 +58,7 @@ class Registry(metaclass=Singleton):
             value (Type[BaseSystemParser]): The system parser implementation.
 
         Raises:
-            ValueError: If the system parser implementation does not exist.
+            ValueError: If value is not a subclass of BaseSystemParser.
         """
         if not issubclass(value, BaseSystemParser):
             raise ValueError(f"Invalid system implementation for '{name}', should be subclass of 'System'.")
@@ -75,8 +88,53 @@ class Registry(metaclass=Singleton):
             value (Type[BaseRunner]): The runner implementation.
 
         Raises:
-            ValueError: If the runner implementation does not exist.
+            ValueError: If value is not a subclass of BaseRunner.
         """
         if not issubclass(value, BaseRunner):
             raise ValueError(f"Invalid runner implementation for '{name}', should be subclass of 'BaseRunner'.")
         self.runners_map[name] = value
+
+    def add_strategy(
+        self,
+        strategy_interface: Type[Union[TestTemplateStrategy, ReportGenerationStrategy, JobIdRetrievalStrategy]],
+        system_types: List[Type[System]],
+        template_types: List[Type[TestTemplate]],
+        strategy: Type[Union[TestTemplateStrategy, ReportGenerationStrategy, JobIdRetrievalStrategy]],
+    ) -> None:
+        for system_type in system_types:
+            for template_type in template_types:
+                key = (strategy_interface, system_type, template_type)
+                if key in self.strategies_map:
+                    raise ValueError(f"Duplicating implementation for '{key}', use 'update()' for replacement.")
+                self.update_strategy(key, strategy)
+
+    def update_strategy(
+        self,
+        key: Tuple[
+            Type[Union[TestTemplateStrategy, ReportGenerationStrategy, JobIdRetrievalStrategy]],
+            Type[System],
+            Type[TestTemplate],
+        ],
+        value: Type[Union[TestTemplateStrategy, ReportGenerationStrategy, JobIdRetrievalStrategy]],
+    ) -> None:
+        if not (
+            issubclass(key[0], TestTemplateStrategy)
+            or issubclass(key[0], ReportGenerationStrategy)
+            or issubclass(key[0], JobIdRetrievalStrategy)
+        ):
+            raise ValueError(
+                "Invalid strategy interface type, should be subclass of 'TestTemplateStrategy' or "
+                "'ReportGenerationStrategy' or 'JobIdRetrievalStrategy'."
+            )
+        if not issubclass(key[1], System):
+            raise ValueError("Invalid system type, should be subclass of 'System'.")
+        if not issubclass(key[2], TestTemplate):
+            raise ValueError("Invalid test template type, should be subclass of 'TestTemplate'.")
+
+        if not (
+            issubclass(value, TestTemplateStrategy)
+            or issubclass(value, ReportGenerationStrategy)
+            or issubclass(value, JobIdRetrievalStrategy)
+        ):
+            raise ValueError(f"Invalid strategy implementation {value}, should be subclass of 'TestTemplateStrategy'.")
+        self.strategies_map[key] = value
