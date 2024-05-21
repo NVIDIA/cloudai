4ab87e6ccb9d90ffa828ee293b01a2203441d934
Andrey Maslennikov <andreyma@nvidia.com>
2024-05-21 13:09:35 +0200

    Test that default Runner implemntations are added to the Registry

--- tests/test_init.py
+++ tests/test_init.py
@@ -7,3 +7,10 @@ def test_system_parsers():
     assert "standalone" in parsers
     assert "slurm" in parsers
     assert len(parsers) == 2
+
+
+def test_runners():
+    runners = Registry().runners_map.keys()
+    assert "standalone" in runners
+    assert "slurm" in runners
+    assert len(runners) == 2
