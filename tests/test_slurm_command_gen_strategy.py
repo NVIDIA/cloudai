6f15cdc564c98e0b48535807bcb29466c5bd3bac
Andrey Maslennikov <andreyma@nvidia.com>
2024-05-29 11:16:45 +0200

    Add tests for _write_sbatch_script()

--- tests/test_slurm_command_gen_strategy.py
+++ tests/test_slurm_command_gen_strategy.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Any, Dict
 
 import pytest
 from cloudai.schema.system import SlurmSystem
@@ -145,3 +146,101 @@ class TestNeMoLauncherSlurmCommandGenStrategy__GenExecCommand:
         )
 
         assert "container_mounts=[value:value]" in cmd
+
+
+class TestWriteSbatchScript:
+    MANDATORY_ARGS = {
+        "job_name": "test_job",
+        "num_nodes": 2,
+        "partition": "test_partition",
+        "node_list_str": "node1,node2",
+    }
+
+    def setup_method(self):
+        self.env_vars_str = "export TEST_VAR=VALUE"
+        self.srun_command = "srun --test test_arg"
+
+    def assert_positional_lines(self, lines: list[str]):
+        assert lines[0] == "#!/bin/bash"
+        assert lines[-6] == ""
+        assert lines[-5] == "export SLURM_JOB_MASTER_NODE=$(scontrol show hostname $SLURM_JOB_NODELIST | head -n 1)"
+        assert lines[-4] == ""
+        assert lines[-3] == self.env_vars_str
+        assert lines[-2] == ""
+        assert lines[-1] == self.srun_command
+
+    @pytest.mark.parametrize("missing_arg", ["job_name", "num_nodes", "partition", "node_list_str"])
+    def test_raises_on_missing_args(self, missing_arg: str, strategy_fixture: SlurmCommandGenStrategy, tmp_path: Path):
+        args = self.MANDATORY_ARGS.copy()
+        del args[missing_arg]
+
+        with pytest.raises(KeyError) as exc_info:
+            strategy_fixture._write_sbatch_script(args, self.env_vars_str, self.srun_command, str(tmp_path))
+        assert f"KeyError('{missing_arg}')" in str(exc_info)
+
+    def test_only_mandatory_args(self, strategy_fixture: SlurmCommandGenStrategy, tmp_path: Path):
+        sbatch_command = strategy_fixture._write_sbatch_script(
+            self.MANDATORY_ARGS, self.env_vars_str, self.srun_command, str(tmp_path)
+        )
+
+        filepath_from_command = sbatch_command.split()[-1]
+        assert sbatch_command == f"sbatch {filepath_from_command}"
+        assert tmp_path.joinpath("cloudai_sbatch_script.sh").exists()
+
+        with open(filepath_from_command, "r") as file:
+            file_contents = file.read()
+
+        lines = file_contents.splitlines()
+        assert len(lines) == 13
+
+        self.assert_positional_lines(lines)
+
+        assert f"#SBATCH --job-name={self.MANDATORY_ARGS['job_name']}" in file_contents
+        assert f"#SBATCH -N {self.MANDATORY_ARGS['num_nodes']}" in file_contents
+        assert f"#SBATCH --partition={self.MANDATORY_ARGS['partition']}" in file_contents
+        assert f"#SBATCH --nodelist={self.MANDATORY_ARGS['node_list_str']}" in file_contents
+        assert f"#SBATCH --output={tmp_path / 'stdout.txt'}" in file_contents
+        assert f"#SBATCH --error={tmp_path / 'stderr.txt'}" in file_contents
+
+    @pytest.mark.parametrize(
+        "arg, arg_value, expected_str",
+        [
+            ("account", "test_account", "#SBATCH --account=test_account"),
+            ("distribution", "block", "#SBATCH --distribution=block"),
+            ("gpus_per_node", 2, "#SBATCH --gpus-per-node=2"),
+            ("ntasks_per_node", 2, "#SBATCH --ntasks-per-node=2"),
+            ("time_limit", "00:30:00", "#SBATCH --time=00:30:00"),
+        ],
+    )
+    def test_extra_args(
+        self, arg: str, arg_value: str, expected_str: str, strategy_fixture: SlurmCommandGenStrategy, tmp_path: Path
+    ):
+        args = self.MANDATORY_ARGS.copy()
+        args[arg] = arg_value
+
+        sbatch_command = strategy_fixture._write_sbatch_script(
+            args, self.env_vars_str, self.srun_command, str(tmp_path)
+        )
+
+        filepath_from_command = sbatch_command.split()[-1]
+        with open(filepath_from_command, "r") as file:
+            file_contents = file.read()
+
+        self.assert_positional_lines(file_contents.splitlines())
+        assert expected_str in file_contents
+
+    @pytest.mark.parametrize("add_arg", ["output", "error"])
+    def test_disable_output_and_error(self, add_arg: str, strategy_fixture: SlurmCommandGenStrategy, tmp_path: Path):
+        args = self.MANDATORY_ARGS.copy()
+        args[add_arg] = "fake"
+
+        sbatch_command = strategy_fixture._write_sbatch_script(
+            args, self.env_vars_str, self.srun_command, str(tmp_path)
+        )
+
+        filepath_from_command = sbatch_command.split()[-1]
+        with open(filepath_from_command, "r") as file:
+            file_contents = file.read()
+
+        self.assert_positional_lines(file_contents.splitlines())
+        assert f"--{add_arg}=" not in file_contents
