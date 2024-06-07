24d469cdc4e12acfa4d8cd441c1d6b639ea1ad78
Srivatsan Krishnan <srivatsank@nvidia.com>
2024-06-07 08:13:52 -0700

    ruffling and linting

--- tests/test_check_copyright_headers.py
+++ tests/test_check_copyright_headers.py
@@ -33,6 +33,7 @@ HEADER = """# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
 PY_FILES = [p for p in Path().rglob("**/*.py") if "venv" not in str(p)]
 HEADER_LINES = HEADER.count("\n") + 1
 
+
 @pytest.mark.parametrize("py_file", PY_FILES, ids=[str(f) for f in PY_FILES])
 def test_check_copyright_header(py_file):
     with open(py_file, "r") as file:
