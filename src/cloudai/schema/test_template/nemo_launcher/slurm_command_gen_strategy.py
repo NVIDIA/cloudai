89a591616299620bfca246746ddbcaf0c9b7aeeb
Taekyung Heo <7621438+TaekyungHeo@users.noreply.github.com>
2024-07-03 14:29:17 -0400

    Fix bug in generating NeMo launcher command

--- src/cloudai/schema/test_template/nemo_launcher/slurm_command_gen_strategy.py
+++ src/cloudai/schema/test_template/nemo_launcher/slurm_command_gen_strategy.py
@@ -88,7 +88,7 @@ class NeMoLauncherSlurmCommandGenStrategy(SlurmCommandGenStrategy):
 
         cmd_args_str = self._generate_cmd_args_str(self.final_cmd_args, nodes)
 
-        full_cmd = f"./venv/bin/python {launcher_path}/launcher_scripts/main.py {cmd_args_str}"
+        full_cmd = f"python {launcher_path}/launcher_scripts/main.py {cmd_args_str}"
 
         if extra_cmd_args:
             full_cmd += " " + extra_cmd_args
