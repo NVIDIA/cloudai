6f90327ada4d433a7796e73258fa9889ccc23923
Andrey Maslennikov <andreyma@nvidia.com>
2024-07-23 11:57:50 +0200

    Add missing file

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: NVIDIA CORPORATION & AFFILIATES
+# Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from cloudai.systems.slurm.slurm_system import SlurmPartition, SlurmSystem
+
+
+@pytest.fixture
+def slurm_system() -> SlurmSystem:
+    system = SlurmSystem(
+        name="test_system",
+        install_path="/fake/path",
+        output_path="/fake/output",
+        default_partition="main",
+        partitions=[
+            SlurmPartition(name="main", nodes=["node-[033-064]"]),
+            SlurmPartition(name="backup", nodes=["node0[1-8]"]),
+        ],
+    )
+    return system
