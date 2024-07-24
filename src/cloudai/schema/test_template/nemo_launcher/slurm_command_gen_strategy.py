bcf93160a8a2e31fa6f709a04d9403a86e40fcbe
jeffnvidia <jmahou@nvidia.com>
2024-07-24 11:55:26 +0300

    add support for NeMoLauncher reservation

--- src/cloudai/schema/test_template/nemo_launcher/slurm_command_gen_strategy.py
+++ src/cloudai/schema/test_template/nemo_launcher/slurm_command_gen_strategy.py
@@ -58,6 +58,10 @@ class NeMoLauncherSlurmCommandGenStrategy(SlurmCommandGenStrategy):
         for key, value in final_env_vars.items():
             self.final_cmd_args[f"env_vars.{key}"] = value
         self.final_cmd_args["cluster.partition"] = self.slurm_system.default_partition
+        reservation_key = "--reservation "
+        if self.slurm_system.extra_srun_args and reservation_key in self.slurm_system.extra_srun_args:
+            reservation = extra_cmd_args.split(reservation_key, 1)[1].split(" ", 1)[0]
+            self.final_cmd_args["+cluster.reservation"] = reservation
         nodes = self.slurm_system.parse_nodes(nodes)
         if nodes:
             self.final_cmd_args["training.trainer.num_nodes"] = str(len(nodes))
