7a9ffeb381816924706532ae2433089fb5b9fc1d
Andrey Maslennikov <andreyma@nvidia.com>
2024-09-06 14:57:36 +0200

    Use ABC for GradingStrategy

--- src/cloudai/_core/grading_strategy.py
+++ src/cloudai/_core/grading_strategy.py
@@ -14,11 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from pathlib import Path
 
 
-class GradingStrategy:
+class GradingStrategy(ABC):
     """Abstract class for grading test performance."""
 
     @abstractmethod
