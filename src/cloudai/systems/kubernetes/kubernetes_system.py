a018fffa0bd3b8d23f7439baa5c447a18f412686
Taekyung Heo <7621438+TaekyungHeo@users.noreply.github.com>
2024-09-17 11:39:33 -0400

    Make vulture happy

--- src/cloudai/systems/kubernetes/kubernetes_system.py
+++ src/cloudai/systems/kubernetes/kubernetes_system.py
@@ -61,7 +61,7 @@ class KubernetesSystem(BaseModel, System):
     _batch_v1: client.BatchV1Api
     _custom_objects_api: CustomObjectsApi
 
-    def model_post_init(self, __context):
+    def model_post_init(self, __context):  # noqa: Vulture
         """Initialize the KubernetesSystem instance."""
         kube_config_path = self.kube_config_path
         if not kube_config_path.is_file():
