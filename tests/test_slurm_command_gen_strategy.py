83b18b9ba82f8ee35c06c96f7a04a2cbc639c023
Taekyung Heo <theo@nvidia.com>
2024-10-01 12:51:15 -0400

    Unset argument in NeMo launcher when its value is ~ (#223)

--- tests/test_slurm_command_gen_strategy.py
+++ tests/test_slurm_command_gen_strategy.py
@@ -398,6 +398,25 @@ class TestNeMoLauncherSlurmCommandGenStrategy__GenExecCommand:
                 nodes=[],
             )
 
+    def test_argument_with_tilde_value(self, nemo_cmd_gen: NeMoLauncherSlurmCommandGenStrategy):
+        extra_env_vars = {"TEST_VAR_1": "value1"}
+        cmd_args = {
+            "docker_image_url": "fake",
+            "repository_url": "fake",
+            "repository_commit_hash": "fake",
+            "training.model.optim.bucket_cap_mb": "~",
+        }
+
+        cmd = nemo_cmd_gen.gen_exec_command(
+            cmd_args=cmd_args,
+            extra_env_vars=extra_env_vars,
+            extra_cmd_args="",
+            output_path=Path(""),
+            num_nodes=1,
+            nodes=[],
+        )
+        assert "~training.model.optim.bucket_cap_mb=null" in cmd
+
 
 class TestWriteSbatchScript:
     MANDATORY_ARGS = {
