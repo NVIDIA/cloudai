488428169ea844af2fa942e8ad33bc7235c89d53
Srivatsank Krishnan <srivatsank@nvidia.com>
2024-10-01 16:09:41 -0700

    revert docker url back

--- src/cloudai/test_definitions/nemo_launcher.py
+++ src/cloudai/test_definitions/nemo_launcher.py
@@ -95,7 +95,7 @@ class NeMoLauncherCmdArgs(CmdArgs):
 
     repository_url: str = "https://github.com/NVIDIA/NeMo-Framework-Launcher.git"
     repository_commit_hash: str = "cf411a9ede3b466677df8ee672bcc6c396e71e1a"
-    docker_image_url: str = "nvcr.io/nvidia/nemo:24.05.01"
+    docker_image_url: str = "nvcr.io/nvidia/nemo:24.01.01"
     stages: str = '["training"]'
     data_dir: str = "~"
     numa_mapping: NumaMapping = Field(default_factory=NumaMapping)
