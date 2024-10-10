156ccf9c13661de74fa2f5c0b257afe4c0751132
Andrey Maslennikov <andreyma@nvidia.com>
2024-10-10 20:25:48 +0200

    Use subcommands instead of `--mode <value>` (#194)
    * Move Parser to a higher level
    
    * Dirty implementation of Pydantic models for Systems
    
    * Remove System Parsers
    
    * Simplify code a bit
    
    * Enable 'groups' parsing
    
    * Handle group name the same way as partition name
    
    * Make ruff happy
    
    * Add missing module
    
    * Fixes
    
    * Add pydantic to requirements
    
    * Fix tests
    
    * Fix tests
    
    * Update test, still fails
    
    * Make it work
    
    * Add missing file
    
    * Test all systems
    
    * Add mode for verifying system TOMLs
    
    * Fixes
    
    * Make ruff happy
    
    * Extend testing
    
    * Fixes
    
    * Update README
    
    * Address review comments
    
    * Initial support for subcommands
    
    * Rework CLI parsing to use subcommands
    
    * Better naming
    
    * Revert accidential changes
    
    * Update docs
    
    * Log install dir for users
    
    * Extend testing and update help messages
    
    * Move setup_logging under cli/
    
    * Update CI to use new CLI
    
    * Expand CI, fix arg names
    
    * Fix CLI tests
    
    * Remove --test-template-dir arg
    
    * Simplify test case
    
    * Reduce complexity of init_default_args
    
    * Fix arg name
    
    * Allow --test-scenario for install|uninstall subcommands

--- tests/test_acceptance.py
+++ tests/test_acceptance.py
@@ -14,14 +14,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 from concurrent.futures import Future
 from pathlib import Path
 from typing import Dict
 from unittest.mock import MagicMock, Mock, patch
 
 import pytest
-from cloudai import BaseInstaller, InstallStatusResult, NcclTest, Parser, Test, TestTemplate, UCCTest
-from cloudai.__main__ import handle_dry_run_and_run, identify_unique_test_templates, setup_logging
+from cloudai import BaseInstaller, InstallStatusResult, NcclTest, Test, TestTemplate, UCCTest
+from cloudai.cli import handle_dry_run_and_run, identify_unique_test_templates, setup_logging
 from cloudai.systems import SlurmSystem, StandaloneSystem
 from cloudai.test_definitions.nccl import NCCLCmdArgs, NCCLTestDefinition
 
@@ -42,12 +43,16 @@ def test_slurm(tmp_path: Path, scenario: Dict):
     log_file = scenario.get("log_file", ".")
     log_file_path = tmp_path / log_file
 
-    parser = Parser(Path("conf/common/system/example_slurm_cluster.toml"))
-    system, tests, test_scenario = parser.parse(Path("conf/common/test"), test_scenario_path)
-    system.output_path = tmp_path
-    assert test_scenario is not None, "Test scenario is None"
     setup_logging(log_file_path, "DEBUG")
-    handle_dry_run_and_run("dry-run", system, tests, test_scenario)
+    args = argparse.Namespace(
+        mode="dry-run",
+        system_config=Path("conf/common/system/example_slurm_cluster.toml"),
+        test_templates_dir=Path("conf/common/test_template"),
+        tests_dir=Path("conf/common/test"),
+        test_scenario=test_scenario_path,
+        output_dir=tmp_path,
+    )
+    handle_dry_run_and_run(args)
 
     # Find the directory that was created for the test results
     results_output_dirs = [d for d in tmp_path.iterdir() if d.is_dir()]
