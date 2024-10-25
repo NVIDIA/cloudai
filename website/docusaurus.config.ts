195f31bd8831c2383c90135532c08991e0aa6b5c
Andrey Maslennikov <andreyma@nvidia.com>
2024-10-25 12:46:09 +0200

    Use official org

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://amaslenn.github.io',
+  url: 'https://nvidia.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/cloudai/',
@@ -16,7 +16,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'amaslenn',
+  organizationName: 'NVIDIA',
   projectName: 'cloudai',
 
   onBrokenLinks: 'throw',
