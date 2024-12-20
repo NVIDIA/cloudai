6e5621fe32f4ff27bce8ad84f75885f76985b790
Andrey Maslennikov <andreyma@nvidia.com>
2024-12-20 17:54:48 +0100

    Make ruff happy

--- src/cloudai/installer/slurm_installer.py
+++ src/cloudai/installer/slurm_installer.py
@@ -38,8 +38,8 @@ class SlurmInstaller(BaseInstaller):
             not require it.
     """
 
-    PREREQUISITES = ["git", "sbatch", "sinfo", "squeue", "srun", "scancel", "sacct"]
-    REQUIRED_SRUN_OPTIONS = [
+    PREREQUISITES = ("git", "sbatch", "sinfo", "squeue", "srun", "scancel", "sacct")
+    REQUIRED_SRUN_OPTIONS = (
         "--mpi",
         "--gpus-per-node",
         "--ntasks-per-node",
