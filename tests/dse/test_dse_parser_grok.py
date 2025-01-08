3e0864d17be1f996e77b6951cb20636378c830d1
Srivatsan Krishnan <srivatsank@nvidia.com>
2025-01-07 23:41:21 -0800

    Add more test to have ranges for FDL flags.

--- tests/dse/test_dse_parser_grok.py
+++ tests/dse/test_dse_parser_grok.py
@@ -122,3 +122,106 @@ def test_grok_cmd_args_with_mixed_values():
     test = Test(test_definition=test_def, test_template=test_template)
 
     assert test.cmd_args == test_def.cmd_args_dict
+
+
+def test_grok_cmd_args_with_list_values():
+    data = {
+        "name": "example Grok",
+        "description": "Example Grok",
+        "test_template_name": "ExampleEnv",
+        "cmd_args": {
+            "fdl": GrokFdl(
+                checkpoint_policy=["save_iteration_input", "save_all"],
+                combine_qkv=[True, False],
+                dcn_mesh_shape=["'[1, 8, 1, 1]'", "'[2, 4, 2, 1]'"],
+                dims_per_head=[128, 256],
+                hidden_dims=[32768, 65536],
+                ici_mesh_shape=["'[1, 1, 8, 1]'", "'[2, 2, 4, 1]'"],
+                max_seq_len=[8192, 16384],
+                model_dims=[6144, 12288],
+                num_experts=[8, 16],
+                num_groups=[64, 128],
+                num_heads=[48, 96],
+                num_kv_heads=[8, 16],
+                num_layers=[64, 128],
+                percore_batch_size=[1.0, 2.0],
+                use_expert_parallel=[True, False],
+                use_fp8=[1, 0],
+                use_te_dpa=[True, False],
+                vocab_size=[131072, 262144],
+            ),
+            "fdl_config": "paxml.tasks.lm.params.nvidia.Grok_Proxy",
+            "enable_pgle": True,
+            "setup_flags": {},
+            "profile": GrokProfileXLAFlags(
+                xla_gpu_disable_async_collectives="ALLREDUCE,ALLGATHER,REDUCESCATTER,COLLECTIVEBROADCAST,ALLTOALL,COLLECTIVEPERMUTE",
+                xla_gpu_run_post_layout_collective_pipeliner=False,
+                xla_gpu_enable_latency_hiding_scheduler=False,
+            ),
+            "perf": GrokPerfXLAFlags(
+                combine_threshold_bytes=[301989888, 401989888, 501989888],
+                xla_gpu_run_post_layout_collective_pipeliner=False,
+                xla_gpu_use_memcpy_local_p2p=False,
+                xla_gpu_pgle_profile_file_or_directory_path="/opt/paxml/workspace/pgle_output_profile.pbtxt",
+            ),
+            "docker_image_url": "docker://example_image",
+            "output_path": "/path/to/output",
+        },
+        "extra_env_vars": {
+            "ENV1": "0",
+            "ENV2": "1",
+            "ENV3": "3221225472",
+        },
+    }
+
+    test_def = GrokTestDefinitionWrapper(**data)
+
+    assert test_def.cmd_args_dict == {
+        "fdl": GrokFdl(
+            checkpoint_policy=["save_iteration_input", "save_all"],
+            combine_qkv=[True, False],
+            dcn_mesh_shape=["'[1, 8, 1, 1]'", "'[2, 4, 2, 1]'"],
+            dims_per_head=[128, 256],
+            hidden_dims=[32768, 65536],
+            ici_mesh_shape=["'[1, 1, 8, 1]'", "'[2, 2, 4, 1]'"],
+            max_seq_len=[8192, 16384],
+            model_dims=[6144, 12288],
+            num_experts=[8, 16],
+            num_groups=[64, 128],
+            num_heads=[48, 96],
+            num_kv_heads=[8, 16],
+            num_layers=[64, 128],
+            percore_batch_size=[1.0, 2.0],
+            use_expert_parallel=[True, False],
+            use_fp8=[1, 0],
+            use_te_dpa=[True, False],
+            vocab_size=[131072, 262144],
+        ),
+        "fdl_config": "paxml.tasks.lm.params.nvidia.Grok_Proxy",
+        "enable_pgle": True,
+        "setup_flags": {},
+        "profile": GrokProfileXLAFlags(
+            xla_gpu_disable_async_collectives="ALLREDUCE,ALLGATHER,REDUCESCATTER,COLLECTIVEBROADCAST,ALLTOALL,COLLECTIVEPERMUTE",
+            xla_gpu_run_post_layout_collective_pipeliner=False,
+            xla_gpu_enable_latency_hiding_scheduler=False,
+        ),
+        "perf": GrokPerfXLAFlags(
+            combine_threshold_bytes=[301989888, 401989888, 501989888],
+            xla_gpu_run_post_layout_collective_pipeliner=False,
+            xla_gpu_use_memcpy_local_p2p=False,
+            xla_gpu_pgle_profile_file_or_directory_path="/opt/paxml/workspace/pgle_output_profile.pbtxt",
+        ),
+        "docker_image_url": "docker://example_image",
+        "output_path": "/path/to/output",
+    }
+
+    mock_system = MockSystem(
+        name="mock_system",
+        scheduler="mock_scheduler",
+        install_path=Path("/mock/install/path"),
+        output_path=Path("/mock/output/path"),
+    )
+    test_template = MockTestTemplate(system=mock_system, name="example_template")
+    test = Test(test_definition=test_def, test_template=test_template)
+
+    assert test.cmd_args == test_def.cmd_args_dict
