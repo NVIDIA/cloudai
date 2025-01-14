fd3b6c90fcf34491737e5f1da938339be9873421
Srivatsan Krishnan <srivatsank@nvidia.com>
2025-01-13 23:09:23 -0800

    Fix the testing code

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -13,39 +13,34 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from unittest.mock import MagicMock
 
 import pytest
 
 from cloudai._core.configurator.agents.grid_search import GridSearchAgent
-from cloudai._core.test_scenario import TestRun
+from cloudai._core.configurator.cloudai_gym import CloudAIGymEnv
 
 
 @pytest.fixture
-def mock_test_run():
+def mock_env():
     """
-    Fixture to provide a mock TestRun object for testing.
+    Fixture to provide a mock CloudAIGymEnv object for testing.
     """
-    test_run = MagicMock(spec=TestRun)
-    test_run.test = MagicMock()
-    test_run.test.cmd_args = {
-        "docker_image_url": "https://docker/url",
-        "iters": [10, 100],
-        "maxbytes": [1024, 2048],
-        "minbytes": [512, 1024, 2048, 4096],
-        "ngpus": [4],
-        "subtest_name": "nccl_test",
-        "warmup_iters": 5,
+    env = MagicMock(spec=CloudAIGymEnv)
+    env.define_action_space.return_value = {
+        "iters": {"type": "int", "values": [10, 100]},
+        "maxbytes": {"type": "int", "values": [1024, 2048]},
+        "minbytes": {"type": "int", "values": [512, 1024, 2048, 4096]},
+        "ngpus": {"type": "int", "values": [4]},
     }
-    return test_run
+    return env
 
 
-def test_grid_search_agent(mock_test_run):
+def test_grid_search_agent(mock_env):
     """
     Test the GridSearchAgent's ability to traverse the action space.
     """
-    agent = GridSearchAgent(mock_test_run)
+    agent = GridSearchAgent(mock_env)
     agent.configure(config={})
 
     combinations = agent.get_all_combinations()
