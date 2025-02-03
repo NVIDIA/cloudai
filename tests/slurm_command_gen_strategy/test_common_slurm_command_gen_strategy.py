ba571f6464d56d683734db742bb63f583c7457f7
Andrei Maslennikov <andreyma@nvidia.com>
2025-02-03 11:12:56 +0100

    Update tests

--- tests/slurm_command_gen_strategy/test_common_slurm_command_gen_strategy.py
+++ tests/slurm_command_gen_strategy/test_common_slurm_command_gen_strategy.py
@@ -20,7 +20,7 @@ from unittest.mock import Mock, create_autospec
 
 import pytest
 
-from cloudai import Test, TestDefinition, TestRun, TestScenario, TestTemplate
+from cloudai import CmdArgs, Installable, Test, TestDefinition, TestRun, TestScenario, TestTemplate
 from cloudai.systems import SlurmSystem
 from cloudai.systems.slurm.strategy import SlurmCommandGenStrategy
 from cloudai.test_definitions.nccl import NCCLCmdArgs, NCCLTestDefinition
@@ -29,6 +29,12 @@ from tests.conftest import create_autospec_dataclass
 
 class MySlurmCommandGenStrategy(SlurmCommandGenStrategy):
     def _container_mounts(self, tr: TestRun) -> List[str]:
+        return []
+
+
+class MyTestDefinition(TestDefinition):
+    @property
+    def installables(self) -> list[Installable]:
         return []
 
 
@@ -41,18 +47,17 @@ def strategy_fixture(slurm_system: SlurmSystem) -> SlurmCommandGenStrategy:
 
 @pytest.fixture
 def testrun_fixture(tmp_path: Path) -> TestRun:
-    mock_test_definition = Mock(spec=TestDefinition)
+    tdef = MyTestDefinition(
+        name="test_job",
+        description="Test description",
+        test_template_name="d",
+        cmd_args=CmdArgs(),
+        extra_env_vars={"TEST_VAR": "VALUE"},
+    )
     mock_test_template = Mock(spec=TestTemplate)
-
-    mock_test_definition.name = "test_job"
-    mock_test_definition.description = "Test description"
-    mock_test_definition.cmd_args_dict = {"test_arg": "test_value"}
-    mock_test_definition.extra_args_str = "extra_arg"
-    mock_test_definition.extra_env_vars = {"TEST_VAR": "VALUE"}
-
     mock_test_template.name = "test_template"
 
-    test = Test(test_definition=mock_test_definition, test_template=mock_test_template)
+    test = Test(test_definition=tdef, test_template=mock_test_template)
 
     return TestRun(name="test_job", test=test, output_path=tmp_path, num_nodes=2, nodes=["node1", "node2"])
 
