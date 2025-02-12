b1272fa9bd5fe9201921323d5518e3dc0fffc26e
Srivatsank Krishnan <srivatsank@nvidia.com>
2025-02-12 11:45:38 -0800

    constaint check property in test definition

--- src/cloudai/_core/test.py
+++ src/cloudai/_core/test.py
@@ -112,3 +112,7 @@ class TestDefinition(BaseModel, ABC):
     @abstractmethod
     def installables(self) -> list[Installable]:
         return []
+
+    @property
+    def constraint_checker(self) -> bool:
+        return True
