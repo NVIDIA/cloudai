978a4ee2a96a95da8cef3a808c4ab1c8fee7df74
Andrei Maslennikov <andreyma@nvidia.com>
2025-03-04 18:07:33 +0100

    Support agent related settings in scenario

--- src/cloudai/models/scenario.py
+++ src/cloudai/models/scenario.py
@@ -38,6 +38,8 @@ class TestSpecModel(BaseModel):
     extra_container_mounts: list[str] = []
     git_repos: list[GitRepo] = []
     nsys: Optional[NsysConfiguration] = None
+    agent: Optional[str] = None
+    agent_steps: Optional[int] = None
 
 
 class TestRunDependencyModel(BaseModel):
