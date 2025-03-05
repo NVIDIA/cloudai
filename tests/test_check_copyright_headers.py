4ccfcb36c0bf14fc30bf81f5d349df3eed3cf186
Andrei Maslennikov <andreyma@nvidia.com>
2025-03-05 12:57:20 +0100

    Make sure copyright year is valid

--- tests/test_check_copyright_headers.py
+++ tests/test_check_copyright_headers.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import subprocess
 from pathlib import Path
 
 import pytest
@@ -41,15 +42,37 @@ PY_FILES += [p for p in Path().glob("./tests/**/*.py")]
 TOML_FILES = [p for p in Path().glob("conf/**/*.toml")] + ["pyproject.toml", ".taplo.toml"]
 
 
+def prepare_copyright_with_year(file: Path) -> str:
+    res = subprocess.run(
+        ["git", "log", "--format=%ad", "--date=format:%Y", "--follow", file],
+        capture_output=True,
+        text=True,
+    )
+    changed_years = res.stdout.splitlines()
+    created_year, last_modified_year = int(changed_years[-1]), int(changed_years[0])
+    if created_year == last_modified_year:
+        return f"# Copyright (c) {created_year} NVIDIA CORPORATION & AFFILIATES. All rights reserved."
+
+    return f"# Copyright (c) {created_year}-{last_modified_year} NVIDIA CORPORATION & AFFILIATES. All rights reserved."
+
+
 @pytest.mark.parametrize("py_file", PY_FILES, ids=[str(f) for f in PY_FILES])
-def test_src_copyright_header(py_file):
-    with open(py_file, "r") as file:
+def test_src_copyright_header(py_file: Path):
+    with py_file.open() as file:
         first_lines = [next(file).strip() for _ in range(HEADER_LINES)]
-    assert "\n".join(first_lines) == HEADER, f"Header mismatch in {py_file}"
+
+    assert (
+        first_lines[0] == "# SPDX-FileCopyrightText: NVIDIA CORPORATION & AFFILIATES"
+    ), "SPDX-FileCopyrightText is not valid"
+    assert first_lines[1] == prepare_copyright_with_year(py_file), "Copyright year is not valid"
+    assert "\n".join(first_lines[2:]) == "\n".join(HEADER.splitlines()[2:]), f"Header mismatch in {py_file}"
 
 
 @pytest.mark.parametrize("toml_file", TOML_FILES, ids=[str(f) for f in TOML_FILES])
 def test_toml_copyright_header(toml_file):
     with open(toml_file, "r") as file:
         first_lines = [next(file).strip() for _ in range(HEADER_LINES)]
-    assert "\n".join(first_lines) == HEADER, f"Header mismatch in {toml_file}"
+
+    assert first_lines[0] == "# SPDX-FileCopyrightText: NVIDIA CORPORATION & AFFILIATES"
+    assert first_lines[1] == prepare_copyright_with_year(toml_file)
+    assert "\n".join(first_lines[2:]) == "\n".join(HEADER.splitlines()[2:]), f"Header mismatch in {toml_file}"
