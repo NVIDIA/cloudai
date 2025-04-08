610542b1f08863a0edb7a26c3ece5b86d04d4e38
Srivatsank Krishnan <srivatsank@nvidia.com>
2025-04-08 16:35:31 -0700

    reciepe method for complex CLI

--- src/cloudai/workloads/nemo_run/cloudai_nemorun.py
+++ src/cloudai/workloads/nemo_run/cloudai_nemorun.py
@@ -15,34 +15,79 @@
 # limitations under the License.
 
 import os
+from datetime import timedelta
+from typing import Optional
 
 import lightning.pytorch as pl
 import nemo_run as run
 import torch
+from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.loggers.wandb import WandbLogger
 from megatron.core.distributed import DistributedDataParallelConfig
 from megatron.core.optimizer import OptimizerConfig
 from nemo import lightning as nl
 from nemo.collections import llm
 from nemo.collections.common.tokenizers.huggingface import AutoTokenizer
+from nemo.collections.common.tokenizers.tokenizer_spec import TokenizerSpec
 from nemo.collections.llm.gpt.data.mock import MockDataModule
-from nemo.collections.llm.gpt.data.packed_sequence import PackedSequenceSpecs
-from nemo.collections.llm.gpt.data.squad import SquadDataModule
-from nemo.collections.llm.gpt.model.nemotron import (
-    Nemotron4Config15B,
-    NemotronModel,
-)
+from nemo.collections.llm.gpt.model.llama import Llama3Config8B, LlamaModel
+from nemo.collections.llm.recipes.llama3_70b import pretrain_recipe as llama3_70b_recipe
+from nemo.collections.llm.recipes.llama31_405b import pretrain_recipe as llama3_405b_recipe
+from nemo.collections.llm.recipes.nemotron3_8b import pretrain_recipe as nemotron3_8b_recipe
+from nemo.collections.llm.recipes.nemotron4_15b import pretrain_recipe as nemotron4_15b_recipe
+from nemo.collections.llm.recipes.nemotron4_340b import pretrain_recipe as nemotron4_340b_recipe
 from nemo.collections.nlp.modules.common.tokenizer_utils import get_nmt_tokenizer
+from nemo.lightning import AutoResume, NeMoLogger
+from nemo.lightning.pytorch.callbacks import ModelCheckpoint
 from nemo.lightning.pytorch.callbacks.garbage_collection import GarbageCollectionCallback
 from nemo.lightning.pytorch.callbacks.megatron_comm_overlap import MegatronCommOverlapCallback
 from nemo.lightning.pytorch.callbacks.nsys import NsysCallback
+from nemo.lightning.pytorch.optim import CosineAnnealingScheduler
 from nemo.utils.exp_manager import TimingCallback
+
+
+@run.cli.factory(is_target_default=True)
+def default_log(
+    dir: Optional[str] = None,
+    name: str = "default",
+    tensorboard_logger: Optional[run.Config[TensorBoardLogger]] = None,
+    wandb_logger: Optional[run.Config[WandbLogger]] = None,
+) -> run.Config[NeMoLogger]:
+    ckpt = run.Config(
+        ModelCheckpoint,
+        save_last=False,
+        save_top_k=10,
+        train_time_interval=run.Config(timedelta, minutes=15),
+        filename="{model_name}--{val_loss:.2f}-{step}-{consumed_samples}",
+    )
+
+    # Default TensorBoard logger if not provided
+    if tensorboard_logger is None:
+        tensorboard_logger = run.Config(TensorBoardLogger, save_dir="tb_logs", name=name)
+
+    return run.Config(
+        NeMoLogger,
+        ckpt=ckpt,
+        name=name,
+        tensorboard=tensorboard_logger,
+        wandb=wandb_logger,
+        log_dir=dir,
+    )
+
+
+@run.cli.factory(is_target_default=True)
+def default_resume(resume_if_exists=True, resume_ignore_no_checkpoint=True) -> run.Config[AutoResume]:
+    return run.Config(
+        AutoResume,
+        resume_if_exists=resume_if_exists,
+        resume_ignore_no_checkpoint=resume_ignore_no_checkpoint,
+    )
 
 
 @run.cli.factory
 @run.autoconvert
-def hf_tokenizer() -> run.Config[AutoTokenizer]:
+def hf_tokenizer_llama3_8b() -> run.Config[AutoTokenizer]:
     model_name = "meta-llama/Meta-Llama-3-8B"
-
     return run.Config(
         AutoTokenizer,
         pretrained_model_name=model_name,
@@ -50,10 +95,65 @@ def hf_tokenizer() -> run.Config[AutoTokenizer]:
     )
 
 
-@run.cli.factory(target=MockDataModule, target_arg="tokenizer")
+@run.cli.factory
 @run.autoconvert
-def null_tokenizer() -> run.Config[AutoTokenizer]:
-    return run.Config(get_nmt_tokenizer, library="null", model_name="NullTokenizer", vocab_size=256000)
+def hf_tokenizer_llama3_70b() -> run.Config[AutoTokenizer]:
+    model_name = "meta-llama/Meta-Llama-3-70B"
+    return run.Config(
+        AutoTokenizer,
+        pretrained_model_name=model_name,
+        use_fast=True,
+    )
+
+
+@run.cli.factory
+@run.autoconvert
+def hf_tokenizer_llama3_405b() -> run.Config[AutoTokenizer]:
+    model_name = "meta-llama/Llama-3.1-405B"
+    return run.Config(
+        AutoTokenizer,
+        pretrained_model_name=model_name,
+        use_fast=True,
+    )
+
+
+@run.cli.factory
+@run.autoconvert
+def hf_tokenizer_nemotron3_8b() -> run.Config[AutoTokenizer]:
+    model_name = "nvidia/nemotron-3-8b"
+    return run.Config(
+        AutoTokenizer,
+        pretrained_model_name=model_name,
+        use_fast=True,
+    )
+
+
+@run.cli.factory
+@run.autoconvert
+def hf_tokenizer_nemotron4_15b() -> run.Config[AutoTokenizer]:
+    model_name = "nvidia/nemotron-4-15b"
+    return run.Config(
+        AutoTokenizer,
+        pretrained_model_name=model_name,
+        use_fast=True,
+    )
+
+
+@run.cli.factory
+@run.autoconvert
+def hf_tokenizer_nemotron4_340b() -> run.Config[AutoTokenizer]:
+    model_name = "nvidia/nemotron-4-340b"
+    return run.Config(
+        AutoTokenizer,
+        pretrained_model_name=model_name,
+        use_fast=True,
+    )
+
+
+@run.cli.factory(target=TokenizerSpec)
+@run.autoconvert
+def null_tokenizer(vocab_size: int = 256000) -> run.Config[TokenizerSpec]:
+    return run.Config(get_nmt_tokenizer, library="null", model_name="NullTokenizer", vocab_size=vocab_size)
 
 
 @run.cli.factory
@@ -85,66 +185,233 @@ def nsys_callbacks() -> list[pl.Callback]:
 
 @run.cli.factory
 @run.autoconvert
-def comms_overlap_callbacks_lora() -> list[pl.Callback]:
+def comms_overlap_callbacks() -> list[pl.Callback]:
     return [
         timing_callback(),
         run.Config(MegatronCommOverlapCallback, tp_comm_overlap=False),
     ]
 
 
-@run.cli.factory
-@run.autoconvert
-def comms_overlap_callbacks_pretrain() -> list[pl.Callback]:
-    return [
-        timing_callback(),
-        run.Config(MegatronCommOverlapCallback, overlap_param_gather_with_optimizer_step=False),
-    ]
-
-
-@run.cli.factory
-@run.autoconvert
-def combined_callbacks_lora() -> list[pl.Callback]:
-    start_step = 5
-    end_step = 10
-    return [
-        timing_callback(),
-        run.Config(
-            MegatronCommOverlapCallback,
-            tp_comm_overlap=False,
-        ),
-        run.Config(GarbageCollectionCallback, gc_interval_train=start_step, gc_interval_val=end_step),
-    ]
-
-
-@run.cli.factory
-@run.autoconvert
-def combined_callbacks_pretrain() -> list[pl.Callback]:
-    start_step = 5
-    end_step = 10
-    return [
-        timing_callback(),
-        run.Config(MegatronCommOverlapCallback, overlap_param_gather_with_optimizer_step=False),
-        run.Config(
-            NsysCallback,
-            start_step=start_step,
-            end_step=end_step,
-        ),
-        run.Config(GarbageCollectionCallback, gc_interval_train=100, gc_interval_val=100),
-    ]
-
-
-@run.cli.factory(target=SquadDataModule, target_arg="packed_sequence_specs")
-@run.autoconvert
-def packed_sequence_data_lora() -> run.Config[PackedSequenceSpecs]:
-    return run.Config(PackedSequenceSpecs, pad_cu_seqlens=False, packed_sequence_size=4096)
-
-
+# LLAMA3 8B Recipe
 @run.cli.factory(target=llm.pretrain)
-@run.autoconvert
-def cloudai_recipe() -> run.Partial:
+def cloudai_llama3_8b_recipe() -> run.Partial:
     recipe = run.Partial(
         llm.pretrain,
-        model=run.Config(NemotronModel, config=run.Config(Nemotron4Config15B)),
+        model=run.Config(LlamaModel, config=Llama3Config8B()),
+        trainer=run.Config(
+            nl.Trainer,
+            devices=8,
+            num_nodes=1,
+            accelerator="gpu",
+            max_steps=10,
+            limit_test_batches=50,
+            limit_val_batches=32,
+            log_every_n_steps=10,
+            accumulate_grad_batches=1,
+            use_distributed_sampler=False,
+            plugins=run.Config(
+                nl.MegatronMixedPrecision,
+                precision="bf16-mixed",
+                params_dtype=torch.bfloat16,
+                pipeline_dtype=torch.bfloat16,
+                autocast_enabled=False,
+                grad_reduce_in_fp32=False,
+            ),
+            strategy=run.Config(
+                nl.MegatronStrategy,
+                tensor_model_parallel_size=1,
+                pipeline_model_parallel_size=1,
+                context_parallel_size=2,
+                virtual_pipeline_model_parallel_size=None,
+                sequence_parallel=False,
+                gradient_as_bucket_view=True,
+                ddp=run.Config(
+                    DistributedDataParallelConfig,
+                    check_for_nan_in_grad=True,
+                    grad_reduce_in_fp32=True,
+                    overlap_grad_reduce=True,
+                    overlap_param_gather=True,
+                    average_in_collective=True,
+                ),
+            ),
+            num_sanity_val_steps=0,
+            val_check_interval=1000,
+            max_epochs=10,
+            callbacks=[
+                run.Config(NsysCallback, start_step=5, end_step=10),
+                run.Config(TimingCallback),
+            ],
+        ),
+        data=run.Config(
+            MockDataModule,
+            seq_length=8192,
+            micro_batch_size=1,
+            global_batch_size=32,
+            tokenizer=hf_tokenizer_llama3_8b(),
+        ),
+        optim=run.Config(
+            nl.MegatronOptimizerModule,
+            config=run.Config(
+                OptimizerConfig,
+                optimizer="adam",
+                lr=3e-4,
+                bf16=True,
+                fp16=False,
+                params_dtype=torch.bfloat16,
+                use_distributed_optimizer=True,
+                weight_decay=0.1,
+                adam_beta1=0.9,
+                adam_beta2=0.95,
+                adam_eps=1e-05,
+                clip_grad=1.0,
+            ),
+            lr_scheduler=run.Config(
+                CosineAnnealingScheduler,
+                warmup_steps=2000,
+                constant_steps=0,
+                min_lr=2.9999999999999997e-05,
+            ),
+        ),
+        resume=default_resume(),
+    )
+    return recipe
+
+
+# LLAMA3 70B Recipe
+@run.cli.factory(target=llm.pretrain)
+@run.autoconvert
+def cloudai_llama3_70b_recipe() -> run.Partial:
+    recipe = run.Partial(
+        llm.pretrain,
+        model=run.Config(llama3_70b_recipe(performance_mode=True)),
+        data=run.Config(
+            MockDataModule,
+            seq_length=8192,
+            micro_batch_size=1,
+            global_batch_size=8,
+            tokenizer=null_tokenizer(),
+        ),
+        trainer=run.Config(
+            nl.Trainer,
+            devices=8,
+            num_nodes=1,
+            accelerator="gpu",
+            max_steps=10,
+            limit_test_batches=50,
+            limit_val_batches=32,
+            log_every_n_steps=10,
+            strategy=run.Config(
+                nl.MegatronStrategy,
+                tensor_model_parallel_size=4,
+                pipeline_model_parallel_size=1,
+                context_parallel_size=1,
+                virtual_pipeline_model_parallel_size=None,
+                sequence_parallel=False,
+                pipeline_dtype=torch.bfloat16,
+                ddp=run.Config(
+                    DistributedDataParallelConfig,
+                    check_for_nan_in_grad=True,
+                    grad_reduce_in_fp32=True,
+                    overlap_grad_reduce=True,
+                    overlap_param_gather=True,
+                ),
+            ),
+            num_sanity_val_steps=0,
+            val_check_interval=1000,
+            max_epochs=10,
+        ),
+        optim=run.Config(
+            nl.MegatronOptimizerModule,
+            config=run.Config(
+                OptimizerConfig,
+                lr=1e-4,
+                bf16=True,
+                params_dtype=torch.bfloat16,
+                use_distributed_optimizer=True,
+                weight_decay=0,
+            ),
+        ),
+        resume=run.Config(
+            nl.AutoResume,
+            resume_if_exists=True,
+            resume_ignore_no_checkpoint=True,
+            resume_past_end=True,
+        ),
+    )
+    return recipe
+
+
+# LLAMA3 405B Recipe
+@run.cli.factory(target=llm.pretrain)
+@run.autoconvert
+def cloudai_llama3_405b_recipe() -> run.Partial:
+    recipe = run.Partial(
+        llm.pretrain,
+        model=run.Config(llama3_405b_recipe(performance_mode=True)),
+        data=run.Config(
+            MockDataModule,
+            seq_length=8192,
+            micro_batch_size=1,
+            global_batch_size=8,
+            tokenizer=null_tokenizer(),
+        ),
+        trainer=run.Config(
+            nl.Trainer,
+            devices=8,
+            num_nodes=1,
+            accelerator="gpu",
+            max_steps=10,
+            limit_test_batches=50,
+            limit_val_batches=32,
+            log_every_n_steps=10,
+            strategy=run.Config(
+                nl.MegatronStrategy,
+                tensor_model_parallel_size=8,
+                pipeline_model_parallel_size=1,
+                context_parallel_size=2,
+                virtual_pipeline_model_parallel_size=None,
+                sequence_parallel=False,
+                pipeline_dtype=torch.bfloat16,
+                ddp=run.Config(
+                    DistributedDataParallelConfig,
+                    check_for_nan_in_grad=True,
+                    grad_reduce_in_fp32=True,
+                    overlap_grad_reduce=True,
+                    overlap_param_gather=True,
+                ),
+            ),
+            num_sanity_val_steps=0,
+            val_check_interval=1000,
+            max_epochs=10,
+        ),
+        optim=run.Config(
+            nl.MegatronOptimizerModule,
+            config=run.Config(
+                OptimizerConfig,
+                lr=1e-4,
+                bf16=True,
+                params_dtype=torch.bfloat16,
+                use_distributed_optimizer=True,
+                weight_decay=0,
+            ),
+        ),
+        resume=run.Config(
+            nl.AutoResume,
+            resume_if_exists=True,
+            resume_ignore_no_checkpoint=True,
+            resume_past_end=True,
+        ),
+    )
+    return recipe
+
+
+# NEMOTRON3 8B Recipe
+@run.cli.factory(target=llm.pretrain)
+@run.autoconvert
+def cloudai_nemotron3_8b_recipe() -> run.Partial:
+    recipe = run.Partial(
+        llm.pretrain,
+        model=run.Config(nemotron3_8b_recipe(performance_mode=True)),
         data=run.Config(
             MockDataModule,
             seq_length=2048,
@@ -199,13 +466,151 @@ def cloudai_recipe() -> run.Partial:
             resume_past_end=True,
         ),
     )
+    return recipe
 
+
+# NEMOTRON4 15B Recipe
+@run.cli.factory(target=llm.pretrain)
+@run.autoconvert
+def cloudai_nemotron4_15b_recipe() -> run.Partial:
+    recipe = run.Partial(
+        llm.pretrain,
+        model=run.Config(nemotron4_15b_recipe(performance_mode=True)),
+        data=run.Config(
+            MockDataModule,
+            seq_length=4096,
+            micro_batch_size=1,
+            global_batch_size=8,
+            tokenizer=null_tokenizer(),
+        ),
+        trainer=run.Config(
+            nl.Trainer,
+            devices=8,
+            num_nodes=1,
+            accelerator="gpu",
+            max_steps=10,
+            limit_test_batches=50,
+            limit_val_batches=32,
+            log_every_n_steps=10,
+            strategy=run.Config(
+                nl.MegatronStrategy,
+                tensor_model_parallel_size=2,
+                pipeline_model_parallel_size=1,
+                context_parallel_size=1,
+                virtual_pipeline_model_parallel_size=None,
+                sequence_parallel=False,
+                pipeline_dtype=torch.bfloat16,
+                ddp=run.Config(
+                    DistributedDataParallelConfig,
+                    check_for_nan_in_grad=True,
+                    grad_reduce_in_fp32=True,
+                    overlap_grad_reduce=True,
+                    overlap_param_gather=True,
+                ),
+            ),
+            num_sanity_val_steps=0,
+            val_check_interval=1000,
+            max_epochs=10,
+        ),
+        optim=run.Config(
+            nl.MegatronOptimizerModule,
+            config=run.Config(
+                OptimizerConfig,
+                lr=1e-4,
+                bf16=True,
+                params_dtype=torch.bfloat16,
+                use_distributed_optimizer=True,
+                weight_decay=0,
+            ),
+        ),
+        resume=run.Config(
+            nl.AutoResume,
+            resume_if_exists=True,
+            resume_ignore_no_checkpoint=True,
+            resume_past_end=True,
+        ),
+    )
+    return recipe
+
+
+# NEMOTRON4 340B Recipe
+@run.cli.factory(target=llm.pretrain)
+@run.autoconvert
+def cloudai_nemotron4_340b_recipe() -> run.Partial:
+    recipe = run.Partial(
+        llm.pretrain,
+        model=run.Config(nemotron4_340b_recipe(performance_mode=True)),
+        data=run.Config(
+            MockDataModule,
+            seq_length=4096,
+            micro_batch_size=1,
+            global_batch_size=8,
+            tokenizer=null_tokenizer(),
+        ),
+        trainer=run.Config(
+            nl.Trainer,
+            devices=8,
+            num_nodes=1,
+            accelerator="gpu",
+            max_steps=10,
+            limit_test_batches=50,
+            limit_val_batches=32,
+            log_every_n_steps=10,
+            strategy=run.Config(
+                nl.MegatronStrategy,
+                tensor_model_parallel_size=8,
+                pipeline_model_parallel_size=1,
+                context_parallel_size=1,
+                virtual_pipeline_model_parallel_size=None,
+                sequence_parallel=False,
+                pipeline_dtype=torch.bfloat16,
+                ddp=run.Config(
+                    DistributedDataParallelConfig,
+                    check_for_nan_in_grad=True,
+                    grad_reduce_in_fp32=True,
+                    overlap_grad_reduce=True,
+                    overlap_param_gather=True,
+                ),
+            ),
+            num_sanity_val_steps=0,
+            val_check_interval=1000,
+            max_epochs=10,
+        ),
+        optim=run.Config(
+            nl.MegatronOptimizerModule,
+            config=run.Config(
+                OptimizerConfig,
+                lr=1e-4,
+                bf16=True,
+                params_dtype=torch.bfloat16,
+                use_distributed_optimizer=True,
+                weight_decay=0,
+            ),
+        ),
+        resume=run.Config(
+            nl.AutoResume,
+            resume_if_exists=True,
+            resume_ignore_no_checkpoint=True,
+            resume_past_end=True,
+        ),
+    )
     return recipe
 
 
 if __name__ == "__main__":
     mode = os.getenv("CLOUDAI_NEMO_TASK")
-    print(f"Running in mode {mode}")
+    model_type = os.getenv("CLOUDAI_MODEL_TYPE", "llama3_8b")
+    print(f"Running in mode {mode} with model {model_type}")
+
+    recipe_mapping = {
+        "llama3_8b": cloudai_llama3_8b_recipe,
+        "llama3_70b": cloudai_llama3_70b_recipe,
+        "llama3_405b": cloudai_llama3_405b_recipe,
+        "nemotron3_8b": cloudai_nemotron3_8b_recipe,
+        "nemotron4_15b": cloudai_nemotron4_15b_recipe,
+        "nemotron4_340b": cloudai_nemotron4_340b_recipe,
+    }
+
     if mode == "pretrain":
         run.cli.main(fn=llm.pretrain)
     elif mode == "finetune":
