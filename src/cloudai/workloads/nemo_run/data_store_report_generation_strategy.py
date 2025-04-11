953ffa1654685f57d04a7a5c3cc1b3c090d840d4
Taekyung Heo <theo@nvidia.com>
2025-04-11 11:46:51 -0400

    Reflect CloudAI team's comments

--- src/cloudai/workloads/nemo_run/data_store_report_generation_strategy.py
+++ src/cloudai/workloads/nemo_run/data_store_report_generation_strategy.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 
 import getpass
+import re
 import socket
 from typing import Any, ClassVar, Dict, cast
 
@@ -45,9 +46,11 @@ class NeMoRunDataStoreReportGenerationStrategy(ReportGenerationStrategy):
         )
         publisher = NeMoRunLLAMARecordPublisher(repository=repository_instance)
         tdef = cast(NeMoRunTestDefinition, self.test_run.test.test_definition)
+        docker_image_url = tdef.cmd_args.docker_image_url
+        s_fw_version = self.extract_version_from_docker_image(docker_image_url)
         raw_data: Dict[str, Any] = {
             "s_framework": "nemo",
-            "s_fw_version": "24.09",  # TODO: extract from docker image
+            "s_fw_version": s_fw_version,
             "s_model": tdef.cmd_args.recipe_name,
             "s_model_size": "",  # TODO: 8b, 13b, 30b, 70b...
             "s_workload": tdef.cmd_args.recipe_name,
@@ -71,7 +74,7 @@ class NeMoRunDataStoreReportGenerationStrategy(ReportGenerationStrategy):
             "d_tokens_per_sec": "",  # TODO: = (global_batch_size*encoder_seq_length/throughput.mean)
             "l_checkpoint_size": None,  # TODO: ./common/nemo/nemo-utils.sh
             "d_checkpoint_save_rank_time": None,  # TODO: ./common/nemo/nemo-utils.sh
-            "s_job_id": "0",  # FIXME: get job id
+            "s_job_id": "0",  # TODO: load from metadata when ready
             "s_job_mode": "training",
             "s_image": tdef.cmd_args.docker_image_url,
             "l_num_nodes": self.test_run.num_nodes,
@@ -82,3 +85,7 @@ class NeMoRunDataStoreReportGenerationStrategy(ReportGenerationStrategy):
             "b_synthetic_dataset": "",
         }
         publisher.publish(raw_data)
+
+    def extract_version_from_docker_image(self, docker_image_url: str) -> str:
+        version_match = re.search(r":(\d+\.\d+(?:\.\w+)?)", docker_image_url)
+        return version_match.group(1) if version_match else "unknown"
