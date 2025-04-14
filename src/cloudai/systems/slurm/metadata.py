576250015cb8ed3a8fe1f6aa44bd32998cdab032
Andrei Maslennikov <andreyma@nvidia.com>
2025-04-14 11:34:36 +0200

    Collect more metadata for slurm jobs

--- src/cloudai/systems/slurm/metadata.py
+++ src/cloudai/systems/slurm/metadata.py
@@ -66,6 +66,10 @@ class MetadataSlurm(BaseModel):
     cluster_name: str
     node_list: str
     num_nodes: str
+    ntasks_per_node: str
+    ntasks: str
+    nnodes: str
+    job_id: str
 
 
 class SlurmSystemMetadata(BaseModel):
