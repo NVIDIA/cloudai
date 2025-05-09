87e3a33d899824a2e1cfcfe1f1d497c918a9a3a2
Taekyung Heo <theo@nvidia.com>
2025-05-09 15:38:18 -0400

    Revert 'Add NeMoRunJobStatusRetrievalStrategy and register it in the strategy registry'

--- src/cloudai/workloads/nemo_run/__init__.py
+++ src/cloudai/workloads/nemo_run/__init__.py
@@ -16,7 +16,6 @@
 
 
 from .data_store_report_generation_strategy import NeMoRunDataStoreReportGenerationStrategy
-from .job_status_retrieval_strategy import NeMoRunJobStatusRetrievalStrategy
 from .nemo_run import Data, Log, LogCkpt, NeMoRunCmdArgs, NeMoRunTestDefinition, Trainer, TrainerStrategy
 from .report_generation_strategy import NeMoRunReportGenerationStrategy
 from .slurm_command_gen_strategy import NeMoRunSlurmCommandGenStrategy
@@ -27,7 +26,6 @@ __all__ = [
     "LogCkpt",
     "NeMoRunCmdArgs",
     "NeMoRunDataStoreReportGenerationStrategy",
-    "NeMoRunJobStatusRetrievalStrategy",
     "NeMoRunReportGenerationStrategy",
     "NeMoRunSlurmCommandGenStrategy",
     "NeMoRunTestDefinition",
