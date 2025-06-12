d9e1d03d3c0afce1c228c40b69e0b54c4a8730e0
Srivatsan Krishnan <srivatsank@nvidia.com>
2025-06-12 09:54:00 -0700

    allow grad_reduce_in_fp32 = True

--- src/cloudai/workloads/nemo_run/cloudai_nemorun.py
+++ src/cloudai/workloads/nemo_run/cloudai_nemorun.py
@@ -775,7 +775,7 @@ def cloudai_llama3_405b_recipe() -> run.Partial:
                 params_dtype=torch.bfloat16,
                 pipeline_dtype=torch.bfloat16,
                 autocast_enabled=False,
-                grad_reduce_in_fp32=True,
+                grad_reduce_in_fp32=False,
 
             ),
             strategy=run.Config(
