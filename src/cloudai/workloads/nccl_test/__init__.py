a73ed08779c07028fe2d837b77d20aa40733035c
Andrei Maslennikov <andreyma@nvidia.com>
2025-08-18 17:15:58 +0200

    Add missing change

--- src/cloudai/workloads/nccl_test/__init__.py
+++ src/cloudai/workloads/nccl_test/__init__.py
@@ -17,7 +17,7 @@
 from .grading_strategy import NcclTestGradingStrategy
 from .kubernetes_json_gen_strategy import NcclTestKubernetesJsonGenStrategy
 from .nccl import NCCLCmdArgs, NCCLTestDefinition
-from .nccl_comparisson_report import NcclComparisonReport, NcclComparisonReportConfig
+from .nccl_comparison_report import NcclComparisonReport, NcclComparisonReportConfig
 from .performance_report_generation_strategy import NcclTestPerformanceReportGenerationStrategy
 from .prediction_report_generation_strategy import NcclTestPredictionReportGenerationStrategy
 from .runai_json_gen_strategy import NcclTestRunAIJsonGenStrategy
