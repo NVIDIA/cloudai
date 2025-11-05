1d998b56b58342f68f783dac0363c2c38ef8c48d
Nick Sarkauskas <nsarkauskas@nvidia.com>
2025-11-06 00:10:23 +0200

    Readd import

--- src/cloudai/workloads/ddlb/ddlb.py
+++ src/cloudai/workloads/ddlb/ddlb.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Optional
+from typing import Optional, Union
 
 from cloudai.core import DockerImage, Installable, JobStatusResult, TestRun
 from cloudai.models.workload import CmdArgs, TestDefinition
