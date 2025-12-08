83157fb5f671d4583527186f94b0f4470de3d6a6
Albert Kowalczyk <akowalczyk@nvidia.com>
2025-12-08 18:12:56 +0200

    fix ut part 2

--- tests/test_init.py
+++ tests/test_init.py
@@ -81,6 +81,7 @@ from cloudai.workloads.sleep import (
     SleepStandaloneCommandGenStrategy,
     SleepTestDefinition,
 )
+from cloudai.workloads.osu_bench import OSUBenchSlurmCommandGenStrategy, OSUBenchTestDefinition
 from cloudai.workloads.slurm_container import SlurmContainerCommandGenStrategy, SlurmContainerTestDefinition
 from cloudai.workloads.triton_inference import (
     TritonInferenceSlurmCommandGenStrategy,
@@ -135,6 +136,7 @@ CMD_GEN_STRATEGIES = {
     (SlurmSystem, BashCmdTestDefinition): BashCmdCommandGenStrategy,
     (SlurmSystem, NixlPerftestTestDefinition): NixlPerftestSlurmCommandGenStrategy,
     (SlurmSystem, NIXLKVBenchTestDefinition): NIXLKVBenchSlurmCommandGenStrategy,
+    (SlurmSystem, OSUBenchTestDefinition): OSUBenchSlurmCommandGenStrategy,
 }
 JSON_GEN_STRATEGIES = {
     (KubernetesSystem, NCCLTestDefinition): NcclTestKubernetesJsonGenStrategy,
@@ -208,7 +210,7 @@ def test_installers():
 
 def test_definitions():
     test_defs = Registry().test_definitions_map
-    assert len(test_defs) == 19
+    assert len(test_defs) == 20
     for tdef in [
         ("UCCTest", UCCTestDefinition),
         ("DDLBTest", DDLBTestDefinition),
@@ -229,6 +231,7 @@ def test_definitions():
         ("BashCmd", BashCmdTestDefinition),
         ("NixlPerftest", NixlPerftestTestDefinition),
         ("NIXLKVBench", NIXLKVBenchTestDefinition),
+        ("OSUBench", OSUBenchTestDefinition),
     ]:
         assert test_defs[tdef[0]] == tdef[1]
 
