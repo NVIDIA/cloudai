89c941fae11336d68fb5843acc5c64b31d9396bf
Andrei Maslennikov <andreyma@nvidia.com>
2025-12-08 12:08:32 +0100

    Rework table printing to make it less confusing

--- src/cloudai/reporter.py
+++ src/cloudai/reporter.py
@@ -202,11 +202,10 @@ class StatusReporter(Reporter):
             table.add_row(*columns)
 
         console = Console()
-        console.print(table)
-
         with console.capture() as capture:
-            console.print(table)
-        logging.debug(capture.get())
+            console.print(table)  # doesn't print to stdout, captures only
+
+        logging.info(capture.get())
 
 
 class TarballReporter(Reporter):
