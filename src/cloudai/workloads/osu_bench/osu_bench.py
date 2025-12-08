9641a774fea2c46a8e3c11e22133a9ad37cd6830
Albert Kowalczyk <akowalczyk@nvidia.com>
2025-12-08 17:01:57 +0200

    Fix for invalid command line arguments

--- src/cloudai/workloads/osu_bench/osu_bench.py
+++ src/cloudai/workloads/osu_bench/osu_bench.py
@@ -16,7 +16,7 @@
 
 from __future__ import annotations
 
-from typing import Union, List, Dict, Literal
+from typing import Union, List, Dict, Optional
 
 from pydantic import Field
 
@@ -26,41 +26,50 @@ from cloudai.models.workload import CmdArgs, TestDefinition
 
 def cli_field(*args, cmdline: str, **kwargs):
     """ Wrapper over pydantic Field to add a cmdline attribute to the field. """
-    return Field(*args, json_schema_extra={"cmdline": cmdline})
+    return Field(*args, json_schema_extra={"cmdline": cmdline}, **kwargs)
 
 
 class OSUBenchCmdArgs(CmdArgs):
     """Command line arguments for a OSU Benchmark test."""
 
-    # https://github.com/forresti/osu-micro-benchmarks/blob/master/mpi/collective/osu_coll.c#L202
-
     docker_image_url: str
+    """ URL of the Docker image to use for the test. """
+
     location: str
     """ Location of the OSU Benchmark binary inside the container. """
 
-    benchmark: str
+    benchmark: Union[str, List[str]]
     """ Name of the benchmark to run. """
 
-    message_len: Union[int, List[int]] = cli_field(default=1024, cmdline="-m")
-    """ Message length for the benchmark. """
+    message_size: Optional[Union[str, List[str]]] = cli_field(default=None, cmdline="-m")
+    """ Message size for the benchmark.
 
-    iterations: int = cli_field(default=10, cmdline="-n")
+    Examples:
+    -m 128    // min = default, max = 128
+    -m 2:128  // min = 2, max = 128
+    -m 2:     // min 2, max = default
+    """
+
+    iterations: Optional[int] = cli_field(default=None, cmdline="-i")
     """ Number of iterations for the benchmark. """
 
-    target: Literal["cpu", "gpu", "both"] = cli_field(default="cpu", cmdline="-r")
-    """ Target for the benchmark. """
+    warmup: Optional[int] = cli_field(default=None, cmdline="-x")
+    """ Number of warmup iterations to skip before timing. """
 
-    #cuda: bool = cli_field(default=False, cmdline="-cuda")
+    mem_limit: Optional[int] = cli_field(default=None, cmdline="-M")
+    """ Per-process maximum memory consumption in bytes. """
 
     @classmethod
-    def get_cmdline(cls, name: str) -> str:
-        return cls.model_fields[name].json_schema_extra["cmdline"]
+    def get_cmdline(cls, field: str) -> str:
+        """ Retrieves the command line argument for the given field. """
+        return cls.model_fields[field].json_schema_extra["cmdline"]
 
     def get_args(self) -> Dict[str, str]:
         """ Retrieve the command line arguments for the OSU benchmark. """
 
         general = ("docker_image_url", "location", "benchmark")
-        return {OSUBenchCmdArgs.get_cmdline(name): value for name, value in self.model_dump().items() if name not in general}
+        arguments = {field: value for field, value in self.model_dump().items() if field not in general}
+        return {OSUBenchCmdArgs.get_cmdline(field): value for field, value in arguments.items() if value is not None}
 
 
 class OSUBenchTestDefinition(TestDefinition):
@@ -73,15 +82,48 @@ class OSUBenchTestDefinition(TestDefinition):
     def docker_image(self) -> DockerImage:
         if not self._osu_image:
             self._osu_image = DockerImage(url=self.cmd_args.docker_image_url)
+
         return self._osu_image
 
     @property
     def installables(self) -> list[Installable]:
-        return [self.docker_image, *self.git_repos]
+        return [self.docker_image]
 
     @property
     def cmd_args_dict(self) -> dict[str, str | list[str]]:
         return self.cmd_args.model_dump(exclude={"docker_image_url", "path_to_benchmark"})
 
     def was_run_successful(self, tr: TestRun) -> JobStatusResult:
+        stdout_path = tr.output_path / "stdout.txt"
+        stderr_path = tr.output_path / "stderr.txt"
+
+        if not stdout_path.is_file():
+            return JobStatusResult(
+                is_successful=False,
+                error_message=(
+                    f"stdout.txt file not found in the specified output directory {tr.output_path}. "
+                    "This file is expected to be created as a result of the OSU Benchmark test run."
+                )
+            )
+
+        with open(stdout_path, 'r') as f:
+            content = f.read()
+
+        if content == "":
+            return JobStatusResult(
+                is_successful=False,
+                error_message=(
+                    f"stdout.txt file is empty in the specified output directory {tr.output_path}. "
+                    f"Please check for fatal errors in {stderr_path}"
+            ))
+
+        if "# Size       Avg Latency(us)   Min Latency(us)   Max Latency(us)  Iterations" not in content:
+            return JobStatusResult(
+                is_successful=False,
+                error_message=(
+                    f"Expected OSU benchmark output marker not found in stdout.txt in {tr.output_path}. "
+                    f"Check for errors in the execution or for a different output format."
+                )
+            )
+
         return JobStatusResult(is_successful=True)
