006a5371a96445eaf4adde16d0241c1be5a4788e
Ivan Podkidyshev <ipodkidyshev@nvidia.com>
2026-02-13 12:38:59 +0100

    fix nccl fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -183,3 +183,42 @@ def dse_tr(slurm_system: SlurmSystem) -> TestRun:
     tr = TestRun(name="dse", test=test_definition, num_nodes=1, nodes=["node1"], iterations=12)
     create_test_directories(slurm_system, tr)
     return tr
+
+
+@pytest.fixture
+def nccl_tr(tmp_path: Path) -> TestRun:
+    test = NCCLTestDefinition(
+        name="nccl",
+        description="desc",
+        test_template_name="t",
+        cmd_args=NCCLCmdArgs(docker_image_url="fake://url/nccl"),
+    )
+    tr = TestRun(name="nccl_test", test=test, num_nodes=2, nodes=[], output_path=tmp_path)
+
+    stdout_content = """# Rank  0 Group  0 Pid 1000 on node1 device  0 [0xaa] NVIDIA H100
+# Rank  1 Group  0 Pid 1001 on node1 device  1 [0xbb] NVIDIA H100
+# Rank  2 Group  0 Pid 1002 on node1 device  2 [0xcc] NVIDIA H100
+# Rank  3 Group  0 Pid 1003 on node1 device  3 [0xdd] NVIDIA H100
+# Rank  4 Group  0 Pid 1004 on node1 device  4 [0xee] NVIDIA H100
+# Rank  5 Group  0 Pid 1005 on node1 device  5 [0xff] NVIDIA H100
+# Rank  6 Group  0 Pid 1006 on node1 device  6 [0x11] NVIDIA H100
+# Rank  7 Group  0 Pid 1007 on node1 device  7 [0x22] NVIDIA H100
+# Rank  8 Group  0 Pid 2000 on node2 device  0 [0xaa] NVIDIA H100
+# Rank  9 Group  0 Pid 2001 on node2 device  1 [0xbb] NVIDIA H100
+# Rank 10 Group  0 Pid 2002 on node2 device  2 [0xcc] NVIDIA H100
+# Rank 11 Group  0 Pid 2003 on node2 device  3 [0xdd] NVIDIA H100
+# Rank 12 Group  0 Pid 2004 on node2 device  4 [0xee] NVIDIA H100
+# Rank 13 Group  0 Pid 2005 on node2 device  5 [0xff] NVIDIA H100
+# Rank 14 Group  0 Pid 2006 on node2 device  6 [0x11] NVIDIA H100
+# Rank 15 Group  0 Pid 2007 on node2 device  7 [0x22] NVIDIA H100
+#
+#                                                              out-of-place                       in-place
+#       size         count      type   redop    root     time   algbw   busbw #wrong     time   algbw   busbw #wrong
+     1000000       1000000     float     sum      -1    1.11   10.10   20.20      0    1.12   10.11   20.21      0
+     2000000       2000000     float     sum      -1    2.22   20.20   30.30      0    2.23   20.21   30.31      0
+     12000000      12000000     float     sum      -1   13.13  120.30  130.40      0   13.14  120.31  130.41      0
+# Avg bus bandwidth    : 111.111
+"""
+    (tr.output_path / "stdout.txt").write_text(stdout_content)
+
+    return tr
